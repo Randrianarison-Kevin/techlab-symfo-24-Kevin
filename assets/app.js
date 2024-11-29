499b3bfe4759e0e9e185694627bb3250966f104d
Randrianarison-Kevin <randrianarison.noavintsoa.kevin@gmail.com>
2024-11-29 11:16:17 +0300

    test react

--- assets/app.js
+++ assets/app.js
@@ -6,19 +6,20 @@ import { registerReactControllerComponents } from '@symfony/ux-react';
  * which should already be in your base.html.twig.
  */
 import './styles/app.css';
+import './bootstrap'
 import { createRoot } from 'react-dom/client';
 import Hello from './react/controllers/Hello';
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
-const context = require.context('./react/controllers', true, /\.(j|t)sx?$/);
-// registerReactControllerComponents(require.context('./react/controllers', true, /\.(j|t)sx?$/));
-console.log(context.keys());
-registerReactControllerComponents(context);
-const rootElement = document.getElementById('root'); 
-if (rootElement) 
-{ 
-    const root = createRoot(rootElement); 
-    root.render(<Hello fullName="Fabien" />); 
-    console.log('Composant Hello rendu dans #root'); 
-} else { 
-    console.error('Élément root introuvable'); 
-}
+// const context = require.context('./react/controllers', true, /\.(j|t)sx?$/);
+registerReactControllerComponents(require.context('./react/controllers', true, /\.(j|t)sx?$/));
+// console.log(context.keys());
+// registerReactControllerComponents(context);
+// const rootElement = document.getElementById('root'); 
+// if (rootElement) 
+// { 
+//     const root = createRoot(rootElement); 
+//     root.render(<Hello fullName="Fabien" />); 
+//     console.log('Composant Hello rendu dans #root'); 
+// } else { 
+//     console.error('Élément root introuvable'); 
+// }
