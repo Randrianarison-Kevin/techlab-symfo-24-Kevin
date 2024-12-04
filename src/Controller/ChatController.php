ec9ad753df1a09aec80392b9110c9a27b6354c91
Randrianarison-Kevin <randrianarison.noavintsoa.kevin@gmail.com>
2024-12-04 16:04:50 +0300

    message

--- src/Controller/ChatController.php
+++ src/Controller/ChatController.php
@@ -2,8 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Message;
+use App\Entity\User;
+use App\Repository\MessageRepository;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
@@ -27,6 +32,38 @@ class ChatController extends AbstractController
 
         return $this->render('chat/start.html.twig',[
             'allUser' => $filterUser,
+        ]);    
+    }
+
+    #[Route('chat/{id}', name : 'app_chat')]
+    public function chat(EntityManagerInterface $entityManagerInterface,MessageRepository $messageRepository, User $user, Request $request) : Response 
+    {
+        $currentUser = $this -> getUser();
+        $conversation = $messageRepository->createQueryBuilder('m') 
+            ->where('m.user = :user OR m.user = :currentUser') 
+            ->setParameter('user', $user) 
+            ->setParameter('currentUser', $currentUser) 
+            ->orderBy('m.createdAt', 'ASC') 
+            ->getQuery() 
+            ->getResult();
+
+        if ($request -> isMethod('POST')) {
+            $content = $request -> request -> get('message');
+            if($content) {
+                $message = new Message();
+                $message -> setContent($content);
+                $message -> setCreatedAt(new \DateTimeImmutable());
+                $message -> setUser($currentUser);
+                $entityManagerInterface -> persist($message);
+                $entityManagerInterface -> flush();
+
+                return $this->redirectToRoute('app_chat', ['id' => $user ->getId()]);
+            }
+        }
+
+        return $this -> render('chat/chat.html.twig',[
+            'user' => $user,
+            'conversation' => $conversation
         ]);    
     }
 }
