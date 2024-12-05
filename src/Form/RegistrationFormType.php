de551ddfdf770925c6701e3d0bf09581a444be6e
Randrianarison-Kevin <randrianarison.noavintsoa.kevin@gmail.com>
2024-12-05 08:20:57 +0300

    styles

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -1,11 +1,11 @@
 <?php
-
 namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,11 +14,22 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('username')
+            ->add('username', TextType::class, [
+                'attr' => ['class' => 'form-control'],
+                'label_attr' => ['class' => 'form-label']
+            ])
             ->add('Password', RepeatedType::class, [
                 'type' => PasswordType::class,
-                'first_options' => ['label' => 'Password'],
-                'second_options' => ['label' => 'Repeat Password']
+                'first_options' => [
+                    'label' => 'Password',
+                    'attr' => ['class' => 'form-control'],
+                    'label_attr' => ['class' => 'form-label']
+                ],
+                'second_options' => [
+                    'label' => 'Repeat Password',
+                    'attr' => ['class' => 'form-control'],
+                    'label_attr' => ['class' => 'form-label']
+                ]
             ])
         ;
     }
